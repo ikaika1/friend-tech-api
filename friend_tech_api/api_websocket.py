b428f6afd3403e2454b314862d420cbee7edd63b
ikaika1 <yutokeitoy@outlook.com>
2023-09-23 00:18:08 +0900

    Update api_websocket.py

--- friend_tech_api/api_websocket.py
+++ friend_tech_api/api_websocket.py
@@ -1,44 +1,63 @@
 import websockets
 import json
+
 from .config import API, AUTHORIZATION_TOKEN
 
-
 class APIBase:
-    def __init__(self) -> None:
-        self.session = None
 
-    async def connect(self):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-        }
-        self.session = await websockets.connect(f"wss://{API}/?authorization={AUTHORIZATION_TOKEN}", extra_headers=headers)
+  def __init__(self) -> None:
+    
+    # WebSocket接続するためのヘッダーを定義
+    self.session = None  
 
-    async def disconnect(self):
-        if self.session:
-            await self.session.close()
-            self.session = None
+  async def connect(self):
+    
+    headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+    }
+    
+    # 認証トークンを付与してWebSocket接続
+    self.session = await websockets.connect(f"wss://{API}/?authorization={AUTHORIZATION_TOKEN}", extra_headers=headers)
 
+  async def disconnect(self):
+    
+    # セッションがあれば切断
+    if self.session:
+      await self.session.close()
+    self.session = None
 
 class NonRest:
-    def __init__(self, api_base: APIBase):
-        self.api_base = api_base
 
-    async def receive(self):
-        return await self.api_base.session.recv()
+  def __init__(self, api_base: APIBase):
+    self.api_base = api_base
 
-    async def close(self):
-        await self.api_base.session.close()
+  # メッセージを受信する
+  async def receive(self):
+    return await self.api_base.session.recv()
 
-    async def ping(self):
-        await self.api_base.session.send(json.dumps({"action": "ping"}))
+  # WebSocket接続を切断
+  async def close(self):
+    await self.api_base.session.close()
 
-    async def sendMessage(self, text: str, imagePaths: list[str], chatRoomId: str, clientMessageId: str):
-        """
-        Args:
-        - text : The text content of the message to be sent.
-        - imagePaths: A list of URLs pointing to images. Though it's not a 
-          legitimate request, the function still works with it.
-        - chatRoomId: The address indicating the chat room ID.
-        - clientMessageId: A random 11-digit string consisting of characters a-z and numbers 0-9, 
-          used as an identifier for the message."""
-        await self.api_base.session.send(json.dumps({"action": "sendMessage", "text": f'\"{text}\"', "imagePaths": imagePaths, "chatRoomId": chatRoomId, "clientMessageId": clientMessageId}))
+  # Pingメッセージを送信  
+  async def ping(self):
+    await self.api_base.session.send(json.dumps({"action": "ping"}))
+
+  # メッセージ送信  
+  async def sendMessage(self, text: str, imagePaths: list[str], chatRoomId: str, clientMessageId: str):
+    """
+    メッセージ送信
+
+    Args:
+      - text : メッセージ本文
+      - imagePaths: 画像URLリスト
+      - chatRoomId: チャットルームID
+      - clientMessageId: メッセージID
+    """
+
+    # SendメッセージをJSONで構築して送信
+    await self.api_base.session.send(json.dumps({"action": "sendMessage", 
+                                                 "text": f\"{text}\"",
+                                                 "imagePaths": imagePaths, 
+                                                 "chatRoomId": chatRoomId,
+                                                 "clientMessageId": clientMessageId}))
