d73bdb0172f314211b0e430562d310db0261a474
ikaika1 <yutokeitoy@outlook.com>
2023-09-23 00:23:27 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,24 +1,39 @@
 from friend_tech_api.client import FriendTechClient, FriendTechWebsocketClient
 import asyncio
 
-
+# WebSocketを使ってメッセージを受信する
 async def receive_messages(client_websocket):
-    while True:
-        message = await client_websocket.non_rest.receive()
-        print(f"{message}")
-
+  while True:
+    # メッセージを受信して表示
+    message = await client_websocket.non_rest.receive() 
+    print(f"{message}")
 
 async def main():
-    client = FriendTechClient()
-    example_user = await client.users.by_id(100000)
-    print(example_user)
 
-    client_websocket = FriendTechWebsocketClient()
-    await client_websocket.initialize()
-    asyncio.create_task(receive_messages(client_websocket))
-    await client_websocket.non_rest.ping()
-    await client_websocket.non_rest.sendMessage("A", [], "0x750add0f18b005c20cdf76236bb0f15429c7aa9a", "0xf00dbabes")
+  # REST APIクライアントを作成
+  client = FriendTechClient()  
 
-    await asyncio.sleep(60 * 60 * 24)
+  # ユーザー情報を取得
+  example_user = await client.users.by_id(100000)
+  print(example_user)
 
+  # WebSocketクライアントを作成
+  client_websocket = FriendTechWebsocketClient()
+
+  # WebSocket接続
+  await client_websocket.initialize()
+
+  # メッセージ受信タスクを作成
+  asyncio.create_task(receive_messages(client_websocket))
+
+  # Ping送信
+  await client_websocket.non_rest.ping()
+
+  # メッセージ送信
+  await client_websocket.non_rest.sendMessage("A", [], "0x750add0f18b005c20cdf76236bb0f15429c7aa9a", "0xf00dbabes")
+
+  # 60秒待機
+  await asyncio.sleep(60 * 60 * 24)
+
+# メインタスク起動  
 asyncio.run(main())
